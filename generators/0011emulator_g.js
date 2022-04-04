4283025b07707a534e86a35e98cdbece8f456a37
scnurealsix <892443733@qq.com>
2022-04-04 19:14:25 +0800

    up

---
+++ generators/0011emulator_g.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Blockly.Python.emulator_init = function () {
+    Blockly.Python.definitions_['os_sys'] = 'import os, sys';
+    Blockly.Python.definitions_['change'] = 'from fzq_scnu import change';
+    var level = this.getFieldValue('DIR1');
+    var code1 = 'fzq = change.Create(level="'+level+'")\n';
+    var code2 = 'fzq.go()\n';
+    var code = code1 + code2;
+    return code;
+};
+
+Blockly.Python.emulator_camera = function () {
+  Blockly.Python.definitions_['fzshijue1'] = 'from fzq_scnu import fzshijue1';
+  var cam_name = Blockly.Python.valueToCode(this, 'cam_name',Blockly.Python.ORDER_ATOMIC);
+  var cam_num = Blockly.Python.valueToCode(this, 'cam_num', Blockly.Python.ORDER_ATOMIC);
+  var code = cam_name + ' = fzshijue1.Img()\n'+ cam_name +'.camera('+cam_num+')\n';
+  return code;
+};
