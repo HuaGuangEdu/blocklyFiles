62d14dfaa8d600d12a122a266ace1d1b558db230
蔡依炼 <87569603+caiyilian@users.noreply.github.com>
2022-04-19 21:41:39 +0800

    Update 0010yuyin_g.js

--- generators/0010yuyin_g.js
+++ generators/0010yuyin_g.js
@@ -129,4 +129,9 @@ Blockly.Python.change_setting_DUI= function () {
     var code = s+'.change_vol_spd_gender_DUI('+vol+', '+spd+', "'+gender+'")\n';
     return code;
 };
-
+Blockly.Python["qMZmUE"]= function () {
+  Blockly.Python.definitions_['control_yuyin']='from control import yuyin';
+ var s=Blockly.Python.valueToCode(this,'s',Blockly.Python.ORDER_ATOMIC);
+  var code=s+'.recordNumberList'+'';
+ return [code, Blockly.Python.ORDER_ATOMIC];
+};
