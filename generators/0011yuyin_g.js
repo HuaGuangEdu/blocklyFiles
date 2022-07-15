497c3c3ba9a11cbe5ed2f87f85b1428663eb48b3
蔡依炼 <87569603+caiyilian@users.noreply.github.com>
2022-07-15 20:47:08 +0800

    Update 0011yuyin_g.js

--- generators/0011yuyin_g.js
+++ generators/0011yuyin_g.js
@@ -26,72 +26,11 @@ Blockly.Python.hardware_raspberrypi_read= function () {
     return code;
 };
 
-Blockly.Python.hardware_raspberrypi_luyin= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-    var yuyin_s_luyin =Blockly.Python.valueToCode(this, 'yuyin_s_luyin', Blockly.Python.ORDER_ATOMIC);
-    var luyin_time =Blockly.Python.valueToCode(this, 'luyin_time', Blockly.Python.ORDER_ATOMIC);
-    var luyin_path =Blockly.Python.valueToCode(this, 'luyin_path', Blockly.Python.ORDER_ATOMIC);
-    var code = yuyin_s_luyin+'.my_record('+luyin_time +','+luyin_path +')\n';
-    return code;
-};
-
-Blockly.Python.hardware_raspberrypi_yuyin_txt= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-    var yuyin_s_stt =Blockly.Python.valueToCode(this, 'yuyin_s_stt', Blockly.Python.ORDER_ATOMIC);
-    var txt =Blockly.Python.valueToCode(this, 'txt', Blockly.Python.ORDER_ATOMIC);
-    var code = yuyin_s_stt+'.stt('+txt+')';
-    return [code, Blockly.Python.ORDER_ATOMIC];
-};
-
-Blockly.Python.hardware_raspberrypi_read_txt= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-     var yuyin_s_tts =Blockly.Python.valueToCode(this, 'yuyin_s_tts', Blockly.Python.ORDER_ATOMIC);
-    var txt_4 =Blockly.Python.valueToCode(this, 'txt_4', Blockly.Python.ORDER_ATOMIC);
-    var filename =Blockly.Python.valueToCode(this, 'filename', Blockly.Python.ORDER_ATOMIC);
-    var code = yuyin_s_tts+'.tts('+txt_4 +','+filename+')\n';
-    return code;
-};
-
-Blockly.Python.hardware_raspberrypi_read_txt_1= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-    var yuyin_s_play =Blockly.Python.valueToCode(this, 'yuyin_s_play', Blockly.Python.ORDER_ATOMIC);
-    var filename_1 =Blockly.Python.valueToCode(this, 'filename_1', Blockly.Python.ORDER_ATOMIC);
-    var code = yuyin_s_play+'.play_music(' +filename_1 + ')\n';
-    return code;
-};
-/*
-Blockly.Python.hardware_raspberrypi_qinyunke= function () {
-   Blockly.Python.definitions_['control_yuyin_1'] = 'import urllib\n'+'import requests';
-   var code = 'def qingyunke(msg):\n'+'   url = \'http://api.qingyunke.com/api.php?key=free&appid=0&msg={}\'.format(urllib.parse.quote(msg))\n'+'   html = requests.get(url)\n'+'   return html.json()["content"]\n';
-   return code;
-};
-Blockly.Python.hardware_raspberrypi_qinyunke_2= function () {
-   var code = 'qingyunke(txt)';
-   return [code, Blockly.Python.ORDER_ATOMIC];
-};
-Blockly.Python.hardware_raspberrypi_qinyunke_3= function () {
-   Blockly.Python.definitions_['control_yuyin_2'] = 'import time\n'+'from mutagen.mp3 import MP3\n';
-   var filename_1 =Blockly.Python.valueToCode(this, 'filename_1', Blockly.Python.ORDER_ATOMIC);
-   var code = 'audio_time = MP3("class/speech/{}".format('+'\''+filename_1+'.mp3'+'\''+'))\n'+'t_1 = audio_time.info.length + 1\n'+'time.sleep(t_1)\n';
-   return code;
-};
-*/
 
 
-Blockly.Python.hardware_raspberrypi_robot_get_text= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-    var s =Blockly.Python.valueToCode(this, 's', Blockly.Python.ORDER_ATOMIC);
-    var txt =Blockly.Python.valueToCode(this, 'txt', Blockly.Python.ORDER_ATOMIC);
-    var code = s+'.chat('+txt+')\n';
-    return code;
-};
 
-Blockly.Python.hardware_raspberrypi_robot_word= function () {
-    Blockly.Python.definitions_['control'] = 'from control import yuyin';
-    var s =Blockly.Python.valueToCode(this, 's', Blockly.Python.ORDER_ATOMIC);
-    var code = s+'.chat_ret';
-    return [code, Blockly.Python.ORDER_ATOMIC];
-};
+
+
 
 
 
